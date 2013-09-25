72d37ed65c8a199f6a7d00fd1d8fc4b6ac225c45
1000ch123 <masa.guruto@gmail.com>
2013-09-25 16:26:32 +0900

    fix construction

--- operationTest.py
+++ operationTest.py
@@ -1,18 +1,22 @@
 #coding:utf-8
 
-# initialize
-import serial
-ser = serial.Serial("/dev/ttyUSB0",57600)
-ser.open()
-
-# setup
-sendBytes([128])
-sendBytes([131])
-
-# led
-sendBytes([139,8,0,255])
 
 # easy sender
 def sendBytes(ls):
   for val in ls:
     ser.write(chr(val))
+
+if __name__ == "__main__"
+  # initialize
+  import serial
+  ser = serial.Serial("/dev/ttyUSB0",57600)
+  ser.open()
+
+  # setup
+  sendBytes([128])
+  sendBytes([131])
+
+  # led
+  sendBytes([139,8,0,255])
+
+
