e3e342656bd5d6322fb66c7d897a8edaf860b7c9
1000ch123 <masa.guruto@gmail.com>
2013-09-28 18:35:07 +0900

    add two_op.py

--- serialTest/two_op.py
+++ serialTest/two_op.py
@@ -12,68 +12,68 @@ def sendBytes(ls):
     #binary = st.pack("!L",val)
     #ser.write(binary)
     #ser.write(chr(val).encode())
-    ser.write(str(val).encode())
+    ser.write(str(val))
 
 def init():
   sendBytes([128,131])
-  print("init:send(128,131)")
+  print "init:send(128,131)"
 
 def led1on():
   sendBytes([139,8,0,255])
-  print("led on:send(139,8,0,255)")
+  print "led on:send(139,8,0,255)"
 
 def go():
   sendBytes([137,0,100,128,0])
   sendBytes([156,1,44])
   sendBytes([137,0,0,128,0])
-  print("moveForward:30cm")
+  print "moveForward:30cm"
 
 def goFast():
   sendBytes([137,0,200,128,0])
   sendBytes([156,1,44])
   sendBytes([137,0,0,128,0])
-  print("moveForwardFast:30cm")
+  print "moveForwardFast:30cm"
 
 def goSlow():
   sendBytes([137,0,50,128,0])
   sendBytes([156,1,44])
   sendBytes([137,0,0,128,0])
-  print("moveForwardFast:30cm")
+  print "moveForwardFast:30cm"
 
 def back():
   sendBytes([137,255,156,128,0])
   sendBytes([156,254,212])
   sendBytes([137,0,0,128,0])
-  print("moveBackward:30cm")
+  print "moveBackward:30cm"
 
 def backFast():
   sendBytes([137,255,56,128,0])
   sendBytes([156,254,212])
   sendBytes([137,0,0,128,0])
-  print("moveBackward:30cm")
+  print "moveBackward:30cm"
 
 def backSlow():
   sendBytes([137,255,206,128,0])
   sendBytes([156,254,212])
   sendBytes([137,0,0,128,0])
-  print("moveBackward:30cm")
+  print "moveBackward:30cm"
 
 def ccw(deg=360):
   sendBytes([137,0,100,0,1])
   sendBytes([157,0,90])
   sendBytes([137,0,0,128,0])
-  print("rotate_CCW")
+  print "rotate_CCW"
 
 def cw(deg=360):
   #ìÆÇ´Ç™Ç®Ç©ÇµÇ¢ÅDíºêiÇµÇƒÇÈ
   sendBytes([137,0,100,255,255])
   sendBytes([157,255,165])
   sendBytes([137,0,0,128,0])
-  print("rotate_CW")
+  print "rotate_CW"
 
 def connect():
   ser = serial.Serial(path,baudRate)
 
 def close():
   ser.close()
-  print("close connection")
+  print "close connection"
