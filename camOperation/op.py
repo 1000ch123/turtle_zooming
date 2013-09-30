72105748b453cc5e4b1bcdf561a2f42f3756748a
1000ch123 <masa.guruto@gmail.com>
2013-09-30 10:51:44 +0900

    fix sleeptime

--- camOperation/op.py
+++ camOperation/op.py
@@ -21,26 +21,26 @@ def led1on():
   print("led on:send(139,8,0,255)")
 
 # lenŽw’è
-def go(length=10):
-  sendBytes([137,0,100,128,0])
+def go(vel=100,length=10):
+  sendBytes([137,0,vel,128,0])
   sendBytes([156,0,length])
   sendBytes([137,0,0,128,0])
   print("move_go:",length)
 
-def back(length=10):
-  sendBytes([137,255,156,128,0])
+def back(vel=100,length=10):
+  sendBytes([137,255,255-vel,128,0])
   sendBytes([156,255,255 - length])
   sendBytes([137,0,0,128,0])
   print("move_back:",length)
 
-def ccw(deg=10):
-  sendBytes([137,0,100,0,1])
+def ccw(vel=100,deg=10):
+  sendBytes([137,0,vel,0,1])
   sendBytes([157,0,deg])
   sendBytes([137,0,0,128,0])
   print("rotate_CCW")
 
-def cw(deg=10):
-  sendBytes([137,0,100,255,255])
+def cw(vel=100,deg=10):
+  sendBytes([137,0,vel,255,255])
   sendBytes([157,255,255-deg])
   sendBytes([137,0,0,128,0])
   print("rotate_CW")
